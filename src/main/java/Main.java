a43574368cc5d69d2a34c80bd06b2e78f91511af
Kostya Net <>
2021-07-30 17:08:14 +0300

    Main cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,13 @@ import utils.MongoUtil;
 
 
 public class Main {
-    private final MongoDatabase db = MongoUtil.connect("alevel");
+    private static final MongoDatabase db = MongoUtil.connect("alevel");
 
     public static void main(String[] args) {
-        new Main().run();
-    }
-
-    private void run() {
         AccountDao accountDao = new AccountDao(db);
         UserDao userDao = new UserDao(db);
         UserService service = new UserService(accountDao, userDao);
         service.run();
     }
+
 }
